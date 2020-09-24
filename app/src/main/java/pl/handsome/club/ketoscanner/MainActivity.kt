528f211323cda6e1def6ed107547515ffd38f797
kwycislo <darxua@gmail.com>
2020-09-24 14:42:14 +0200

    Initial commit

---
+++ app/src/main/java/pl/handsome/club/ketoscanner/MainActivity.kt
@@ -0,0 +1,13 @@
+package pl.handsome.club.ketoscanner
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
