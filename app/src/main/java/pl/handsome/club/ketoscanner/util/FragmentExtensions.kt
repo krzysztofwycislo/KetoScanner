32b4345b76244c54fd40d7b7088a73da8ab878ed
darx <krzysztofwycislo@gmail.com>
2020-11-23 17:40:01 +0100

    Added fragment transition

--- app/src/main/java/pl/handsome/club/ketoscanner/util/FragmentExtensions.kt
+++ app/src/main/java/pl/handsome/club/ketoscanner/util/FragmentExtensions.kt
@@ -2,8 +2,18 @@ package pl.handsome.club.ketoscanner.util
 
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavDirections
+import androidx.navigation.NavOptions
 import androidx.navigation.fragment.findNavController
+import pl.handsome.club.ketoscanner.R
+
+
+val defaultAnimationOptions = NavOptions.Builder()
+    .setEnterAnim(R.anim.nav_enter_anim)
+    .setExitAnim(R.anim.nav_exit_anim)
+    .setPopEnterAnim(R.anim.nav_pop_enter_anim)
+    .setPopExitAnim(R.anim.nav_pop_exit_anim)
+    .build()
 
 fun Fragment.safeNavigateTo(directions: NavDirections) = with(findNavController()) {
-    currentDestination?.getAction(directions.actionId)?.let { navigate(directions) }
+    currentDestination?.getAction(directions.actionId)?.let { navigate(directions, defaultAnimationOptions) }
 }
