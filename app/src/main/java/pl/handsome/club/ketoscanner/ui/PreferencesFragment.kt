d460a07b23c3797adfdee8c58e0100e4d6e676c9
darx <krzysztofwycislo@gmail.com>
2020-11-25 12:51:37 +0100

    Removed un used fragment

--- app/src/main/java/pl/handsome/club/ketoscanner/ui/PreferencesFragment.kt
+++
@@ -1,7 +0,0 @@
-package pl.handsome.club.ketoscanner.ui
-
-import androidx.fragment.app.Fragment
-import pl.handsome.club.ketoscanner.R
-
-
-class PreferencesFragment : Fragment(R.layout.preferences_fragment)
